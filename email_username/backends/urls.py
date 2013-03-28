b5605b9dd40ef9a906e6d6ab0a9821fd95d4465f
mwieler <mw166301@gmail.com>
2013-03-28 16:08:06 -0400

    minimal /accounts/profile page

--- email_username/backends/urls.py
+++ email_username/backends/urls.py
@@ -48,4 +48,6 @@ urlpatterns = patterns('',
                            TemplateView.as_view(template_name='registration/registration_closed.html'),
                            name='registration_disallowed'),
                        (r'', include('registration.auth_urls')),
+                       url(r'^profile/$',
+                          TemplateView.as_view(template_name='email_username/user_profile.html'))
                        )
